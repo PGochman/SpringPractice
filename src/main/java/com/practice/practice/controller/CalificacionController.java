28f9564dd959c3db5ae5cf21de402ea2a928628a
PGochman <pedrogoch55@gmail.com>
2024-03-05 11:04:13 -0300

    cositas

--- src/main/java/com/practice/practice/controller/CalificacionController.java
+++ src/main/java/com/practice/practice/controller/CalificacionController.java
@@ -6,6 +6,7 @@ import com.practice.practice.service.CalificacionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
